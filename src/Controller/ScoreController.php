6256c6564579e7945a13bf8316b777698be7c01f
leofrk2 <45242248+leofrk2@users.noreply.github.com>
2022-04-04 04:28:22 +0200

    Correction

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Score;
+use App\Form\ConnexionType;
 use App\Form\ScoreType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,23 +17,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ScoreController extends AbstractController
 {
     #[Route('/list', name: '_list')]
-    public function listAction(ManagerRegistry $doctrine): Response
+    public function listAction(ManagerRegistry $doctrine, Session $session): Response
     {
-        $em = $doctrine->getManager();
-        $scoreRepository = $em->getRepository('App:Score');
-        $scores = $scoreRepository->findAll();
-
-        //on affiche les scores dans l'ordre décroissant
-        usort($scores, function ($a, $b) {
-            return $b->getScore() <=> $a->getScore();
-        });
-        if(count($scores) == 0){
-            return $this->render('Accueil/score.html.twig');
-        }
-        $args = [
-            'scores' => $scores,
-        ];
-        return $this->render('Accueil/score.html.twig', $args);
+        $session->set('formConnexionVisible', false);
+        return $this->getScores($doctrine, $session);
     }
 
     #[Route('/add', name: '_add')]
@@ -47,9 +35,66 @@ class ScoreController extends AbstractController
             $em->flush();
             $this->addFlash('success', 'Score ajouté');
             return $this->redirectToRoute('score_list');
-
     }
 
+    #[Route('/connexion', name: '_connexion')]
+    public function connexionAction(Request $request, Session $session, ManagerRegistry $doctrine): Response
+    {
+        $password = $request->get('password');
+        if(sha1($password) == "d033e22ae348aeb5660fc2140aec35850c4da997"){
+            $session->set('isAuth', true);
+            $this->addFlash('info', 'Vous êtes connecté en tant qu\'admin');
+        }
+
+        $session->set('formConnexionVisible', true);
+        return $this->getScores($doctrine, $session);
+    }
+
+    /**
+     * @param ManagerRegistry $doctrine
+     * @param Session $session
+     * @return Response
+     */
+    public function getScores(ManagerRegistry $doctrine, Session $session): Response
+    {
+        $em = $doctrine->getManager();
+        $scoreRepository = $em->getRepository('App:Score');
+        $scores = $scoreRepository->findAll();
+
+        //on affiche les scores dans l'ordre décroissant
+        usort($scores, function ($a, $b) {
+            return $b->getScore() <=> $a->getScore();
+        });
+        if (count($scores) == 0) {
+            return $this->render('Accueil/score.html.twig');
+        }
+        $args = [
+            'scores' => $scores,
+        ];
+        return $this->render('Accueil/score.html.twig', $args);
+    }
+
+    #[Route('/vider', name: '_vider')]
+    public function viderAction(ManagerRegistry $doctrine, Session $session): Response
+    {
+        $em = $doctrine->getManager();
+        $scoreRepository = $em->getRepository('App:Score');
+        $scores = $scoreRepository->findAll();
+        foreach ($scores as $score) {
+            $em->remove($score);
+        }
+        $em->flush();
+        $this->addFlash('info', 'Score supprimé');
+        return $this->redirectToRoute('score_list');
+    }
+
+    #[Route('/deconnexion', name: '_deconnexion')]
+    public function deconnexionAction(Session $session): Response
+    {
+        $session->set('isAuth', false);
+        $this->addFlash('info', 'Vous êtes déconnecté');
+        return $this->redirectToRoute('score_list');
+    }
 
 
 }
